733561b784d52c8b8ed988bd104d8e495bb09d96
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-26 09:25:46 +0200

    initial new learning project

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <Button title="Add item" />
+        <TextInput style={styles.textInput} placeholder="Enter item..." />
+      </View>
+      <View>
+        <Text>רשימת פריטים...</Text>
+      </View>
+
+      <View style={styles.boxContainer}>
+        <View style={styles.box1}>
+          <Text>1</Text>
+        </View>
+        <View style={styles.box2}>
+          <Text>2</Text>
+        </View>
+        <View style={styles.box3}>
+          <Text>3</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 15,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: "space-between"
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#999',
+    width: '80%'
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    width: '80%',
+    height: 300,
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+    borderWidth: 2,
+    marginTop: 25
+  },
+  box1: {
+    backgroundColor: '#f00',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box2: {
+    backgroundColor: '#0f0',
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box3: {
+    backgroundColor: '#00f',
+    flex: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Learning React Native
+
+Course: React Native - The Practical Guide (2023) by Maximilian Schwarzmuller\
+
+- [Link to the course](https://www.udemy.com/course/react-native-the-practical-guide/)
+
+## The course content
+
+- [x] Basic elements of RN.
+- [ ] Debugging.
+- [ ] Components, Layouts & Styling.
+- [ ] Adaptive User Interface.
+- [ ] Navigation with React Navigation.
+- [ ] State managment with Redux & Context API.
+- [ ] Handling user input.
+- [ ] Sending HTTP requests.
+
+## Documents, guides and other relevat links
+
+- [React Native - Official guide](https://reactnative.dev/docs/getting-started)
+- [Expo - Official guide](https://docs.expo.dev/)
+- [Expo Go application for android](https://play.google.com/store/apps/details?id=host.exp.exponent&hl=en&gl=US)
+- [Expo Go application for iOS](https://apps.apple.com/us/app/expo-go/id982107779)
